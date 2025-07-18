f491ef1c31f74d8d1fef56364ba75a7cde60424e
Vignesh Kalyanasundaram <vignesh20092001@gmail.com>
2025-07-18 11:40:13 -0700

    Creative feature added with background color

--- clicker.js
+++ clicker.js
@@ -10,17 +10,21 @@ button.addEventListener('click', function(){
 
     if (number===10) {
         output.textContent = `You're just getting started!`
+        document.body.style.backgroundColor = `pink`
+        
     } 
 
     if (number===25) {
         output.textContent = `Woah! That's a lot of cookies!`
+        document.body.style.backgroundColor = `#d13b38ff`
     } 
-
     if (number===50) {
         output.textContent = `Are you still clicking?`
+        document.body.style.backgroundColor = `red`        
     } 
 
     if (number===100) {
         output.textContent = `You win! Maybe take a break?`
+        document.body.style.backgroundColor = `gray`        
     } 
 });
