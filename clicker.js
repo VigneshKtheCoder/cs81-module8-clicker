d7833b4ae40db3826fd4a0c7082781f0fb03293c
Vignesh Kalyanasundaram <vignesh20092001@gmail.com>
2025-07-18 11:34:23 -0700

    milestone messages

--- clicker.js
+++ clicker.js
@@ -7,4 +7,20 @@ button.addEventListener('click', function(){
     event.preventDefault();
     number++;
     count.textContent = number; 
+
+    if (number===10) {
+        output.textContent = `You're just getting started!`
+    } 
+
+    if (number===25) {
+        output.textContent = `Woah! That's a lot of cookies!`
+    } 
+
+    if (number===50) {
+        output.textContent = `Are you still clicking?`
+    } 
+
+    if (number===100) {
+        output.textContent = `You win! Maybe take a break?`
+    } 
 });
